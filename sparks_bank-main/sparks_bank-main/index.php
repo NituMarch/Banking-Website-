592eb69342c1cdad3b9bd94da034bf36f8c89351
NituMarch <70215060+NituMarch@users.noreply.github.com>
2022-07-19 22:17:21 +0530

    Add files via upload

---
+++ sparks_bank-main/sparks_bank-main/index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to Sparks Foundation Bank!!!</h1>
+
+
+  <div class="centre">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">VIEW USERS</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">TRANSFER AMOUNT</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="blue">TRANSFER HISTORY</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
